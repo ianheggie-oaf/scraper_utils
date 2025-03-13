a054485f47bec73377c9fcd87b70d653a51d8b1e
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-03-14 00:11:25 +1100

    Removed specs with no actual tests

--- spec/scraper_utils/replacements2/mechanize_actions_spec.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "../../spec_helper"
-require_relative "../replacements/mechanize_actions_replacements_spec"
--- spec/scraper_utils/selectors2/mechanize_actions_spec.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "../../spec_helper"
-require_relative "../replacements/hanize_actions_selectors_spec"
